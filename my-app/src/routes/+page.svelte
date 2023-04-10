977ea3662773f68ac7027a6b092e174c0f9302cc
calz18 <40797702+calz18@users.noreply.github.com>
2023-04-10 12:38:48 +0530

    test

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,7 +1,8 @@
-<script>
+<!-- <script>
   import { onMount } from "svelte";
 
   let response = {};
+
   let array = [];
   [...Array(5).keys()].forEach((getData) => console.log(Array(getData)));
   const getData = async () => {
@@ -18,7 +19,7 @@
         body: JSON.stringify({ bet }),
       }
     );
-    console.log(res);
+    /*  console.log(res); */
     let apiResponse = await res.json();
     console.log(apiResponse);
     response = {
@@ -35,73 +36,62 @@
   <button on:click={getData}>Run</button>
 </div>
 
-<!-- <script>
-  import { onMount } from 'svelte';
-  import { Line } from 'svelte-chartjs';
-
-  let slotMachineData = [];
-  let graphData = {
-    labels: [],
-    datasets: [
-      {
-        label: 'Wins',
-        data: [],
-        borderColor: 'blue',
-        fill: false
-      },
-      {
-        label: 'Losses',
-        data: [],
-        borderColor: 'red',
-        fill: false
-      }
-    ]
-  };
-
-  onMount(async () => {
-    const response = await fetch('https://us-central1-ccc-slots.cloudfunctions.net/api/v1');
-    const data = await response.json();
-    slotMachineData = data.slotMachineData;
-    graphData.labels = data.graphData.labels;
-    graphData.datasets[0].data = data.graphData.wins;
-    graphData.datasets[1].data = data.graphData.losses;
-  });
-</script>
-
-<main>
-  <h1>Slot Machine Data</h1>
-  <table>
-    <thead>
-      <tr>
-        <th>Spin ID</th>
-        <th>Result</th>
-        <th>Win/Loss</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each slotMachineData as spin}
-        <tr>
-          <td>{spin.spinId}</td>
-          <td>{spin.result}</td>
-          <td>{spin.winLoss}</td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
-  <h1>Graph Data</h1>
-  <Line {data=graphData} />
-</main> -->
-
 <style>
   .box {
-    position: relative;
-    right: 4 rem;
-    top: 4 rem;
+    border-radius: 16px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+    padding: 24px;
+    background-color: #fff;
+  }
+</style>
+ -->
+
+<script>
+  let grid = [];
+  let trips = [];
+  let pairs = [];
+  let reward = 0;
+
+  const fetchData = async () => {
+    // fetch data from API endpoint
+    const response = await fetch(
+      "https://us-central1-ccc-slots.cloudfunctions.net/api/v2"
+    );
+    const data = await response.json();
+    console.log(data);
+    // update variables with new data
+    grid = data.grid;
+    trips = data.matches.trips;
+    pairs = data.matches.pairs;
+    reward = data.reward;
+  };
+</script>
+
+<div class="card" />
+
+<style>
+  .card {
+    background-color: #fff;
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
     padding: 20px;
-    width: 400px;
-    height: 400px;
-    border-radius: 50px;
-    background: #18c0c3;
-    box-shadow: inset -28px -28px 56px #14a3a6, inset 28px 28px 56px #1cdde0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  button {
+    margin-top: 20px;
+    padding: 10px 20px;
+    border-radius: 5px;
+    border: none;
+    background-color: #3366cc;
+    color: #fff;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #265799;
   }
 </style>
