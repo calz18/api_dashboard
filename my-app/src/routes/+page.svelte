657a8a341baf212650f1cd4774044477b5fe6d2c
calz18 <40797702+calz18@users.noreply.github.com>
2023-04-17 02:05:20 +0530

    testing payout

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,10 +1,21 @@
 <!-- <script>
   import { onMount } from "svelte";
+<<<<<<< Updated upstream
 
+=======
+  const rewardsArray = [];
+  let grid = [];
+  let matches = null;
+>>>>>>> Stashed changes
   let response = {};
 
   let array = [];
+<<<<<<< Updated upstream
   [...Array(5).keys()].forEach((getData) => console.log(Array(getData)));
+=======
+  let reward = 0;
+  /*   [...Array(5).keys()].forEach((getData) => console.log(Array(getData))); */
+>>>>>>> Stashed changes
   const getData = async () => {
     let bet = 100;
     const res = await fetch(
@@ -22,17 +33,86 @@
     /*  console.log(res); */
     let apiResponse = await res.json();
     console.log(apiResponse);
+<<<<<<< Updated upstream
     response = {
       currentPayout: apiResponse.payout,
       slotText: apiResponse.result,
     };
+=======
+
+    (grid = apiResponse.grid), (matches = apiResponse.matches);
+    reward = apiResponse.reward;
+    /* grid = apiResponse.grid; */
+
+    /*  const another = { ...matches };
+    for (let key in another) {
+      console.log(key, another[key]);
+    }
+    console.log(another.pairs.reward); */
+
+    matches.pairs.forEach((pair) => {
+      rewardsArray.push(pair.reward);
+    });
+    matches.trips.forEach((trip) => {
+      rewardsArray.push(trip.reward);
+    });
+
+    const sumOfRewards = rewardsArray.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    console.log(rewardsArray); // output: [15, 30]
+    console.log(sumOfRewards); // output: 45
+>>>>>>> Stashed changes
   };
   onMount(getData);
 </script>
 
+<<<<<<< Updated upstream
 <div class="box">
   <h1>Combination: {response.slotText}</h1>
   <h1>payout: {response.currentPayout ?? ""}</h1>
+=======
+<div class="card">
+  <div>
+    <table>
+      {#each grid as row, i}
+        <!--  {#if matches.pairs.some( (pair) => pair.position.some( (pos) => pos.includes(i)) ) }
+          <h1>true</h1>
+        {/if} -->
+        <tr>
+          {#each row as cell, j}
+            <td
+              class={matches.pairs.some((pair) =>
+                pair.position.some((pos) => pos[0] === i && pos[1] === j)
+              ) ||
+              matches.trips.some((trip) =>
+                trip.position.some((pos) => pos[0] === i && pos[1] === j)
+              )
+                ? "highlight"
+                : ""}>{cell}</td
+            >
+          {/each}
+        </tr>
+      {/each}
+    </table>
+  </div>
+
+  <div>
+    <h2>Matches :</h2>
+    {#if matches}
+      <ul>
+        <li>Trips: {JSON.stringify(matches.trips)}</li>
+        <li>Pairs: {JSON.stringify(matches.pairs)}</li>
+      </ul>
+    {/if}
+  </div>
+
+  <h1>
+    Total payout:{sumOfRewards}
+  </h1>
+  <!-- <h1>payout: {response.reward ?? ""}</h1> -->
+>>>>>>> Stashed changes
   <button on:click={getData}>Run</button>
 </div>
 
